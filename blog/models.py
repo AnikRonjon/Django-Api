2cbece88c9ae4dc86d6d4d154163a97b3adb1d70
AptYam <anikronjon.swe@gmail.com>
2021-08-03 14:47:06 +0600

    Blog models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create Models
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    create = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse('api:post_detail', args=[self.publish.year,
+    #                                             self.publish.month,
+    #                                             self.publish.day,
+    #                                             self.slug])
--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls', namespace='api'))
+    path('api/', include('api.urls', namespace='api')),
+    path('', include('blog.urls', namespace='blog')),
 ]
 
 if settings.DEBUG:
