7bef89b01e465a2939b3971096ba1300a55545bc
AptYam <anikronjon.swe@gmail.com>
2021-08-04 07:28:45 +0600

    Bug Fixed Graphql Retrive Query Created

--- core/schema.py
+++ core/schema.py
@@ -6,8 +6,8 @@ class Query(schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(schema.Mutation, graphene.ObjectType):
-    pass
+# class Mutation(schema.Mutation, graphene.ObjectType):
+#     pass
 
 
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema(query=Query)
