aec5dd39193bc274a7476a43deed27e3c066d929
AptYam <anikronjon.swe@gmail.com>
2021-08-06 14:32:44 +0600

    get_absolute_url() with function view

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from .import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail_view, name='post_detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -11,3 +11,12 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.objects.filter(status='published')
         return context
+
+
+def post_detail_view(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post_detail.html', {'post': post})
