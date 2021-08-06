1eb9b4884af77b426ed404adce041f189ea8c427
AptYam <anikronjon.swe@gmail.com>
2021-08-06 13:35:55 +0600

    Template context created

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'blog/post_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(status='published')
+        return context
