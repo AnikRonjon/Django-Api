ecb73c59ba04522e05d47daf4b082346722ca410
Anik Ronjon <anik35-497@diu.edu.bd>
2022-08-16 22:51:44 +0600

    function base @api_view (get request only)

--- api/rest/pagination.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.pagination import LimitOffsetPagination
-
-
-class MyPagination(LimitOffsetPagination):
-    default_limit = 1
-    limit_query_param = 'l'
-    offset_query_param = 'o'
---
+++ basic/blog/views.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category, Post
+from .serializer import CategorySerializer, PostSerializer
+
+
+# Create your views here.
+@api_view()
+def posts_view(request):
+    posts = Post.objects.all()
+    serialize = PostSerializer(posts, many=True)
+    return Response(serialize.data)
